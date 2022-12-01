0f177afc0d99d7d61fdf9ecfd658c1e69e9ed5da
IvanRobles <ivanroblesmvp@gmail.com>
2022-12-01 03:53:31 -0800

    test

--- web/src/public/js/main.js
+++ web/src/public/js/main.js
@@ -859,7 +859,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("current page: ", html_page);
     }
     //Only for the main landing page
-    if (html_page == "index.html") {
+    if (html_page == "") {
         homePageStarter();
     } else if ( html_page == "visual-resume.html") {
         resumePageStarter();
